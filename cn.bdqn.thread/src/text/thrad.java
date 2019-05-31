1ab8da215ff5d36d8e648cc919cbb745cc55b4bb
ycs652459 <652459753@qq.com>
2019-05-31 23:03:45 +0800

    我的自主练习

---
+++ cn.bdqn.thread/src/text/thrad.java
@@ -0,0 +1,18 @@
+package MyThrad;
+
+import java.sql.Time;
+import java.util.jar.Attributes.Name;
+
+/**
+ * 
+ * 爬山類
+ * 
+ * @author Larson
+ *
+ */
+public class thrad extends Thread {
+
+	private int Time;// 爬100所需的時間
+	public int num = 0;// 爬多少個100米
+
+}
